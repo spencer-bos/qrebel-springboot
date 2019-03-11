5225ab85197032bce4f8df084ad18b48456a674a
spencer.bos <spencer.bos@roguewave.com>
2019-03-11 15:10:49 -0400

    changing the controller

--- src/main/java/org/springframework/samples/petclinic/supplement/SupplementController.java
+++ src/main/java/org/springframework/samples/petclinic/supplement/SupplementController.java
@@ -37,9 +37,9 @@ public class SupplementController {
 		LOG.debug(results.toString());
 
 //        DEMO: Show Exception is thrown
-/*        if (true) {
+        if (true) {
             throw new RuntimeException("KEYBOARD NOT FOUND, PRESS F1 TO CONTINUE");
-        }*/
+        }
         return "supplements/supplementList";
     }
 
@@ -70,11 +70,11 @@ public class SupplementController {
     private String getRemoteSupplementsJson() {
         StringBuilder sb = new StringBuilder();
 //        DEMO: Show slow request example
-/*        try {
+       try {
             Thread.sleep(13000); // fake delay
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
 
         try {
             String spec = "http://supplements-service:8080/supplements/";
