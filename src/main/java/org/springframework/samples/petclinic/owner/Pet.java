4aa2c910b5daa100b52fea8a1409724ed7455474
spencer.bos <spencer.bos@roguewave.com>
2019-09-10 08:27:10 -0400

    changing the supplement page

--- src/main/java/org/springframework/samples/petclinic/owner/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Pet.java
@@ -61,7 +61,7 @@ public class Pet extends NamedEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "petId", fetch = FetchType.EAGER)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "petId", fetch = FetchType.LAZY)
     private Set<Visit> visits = new LinkedHashSet<>();
 
     public void setBirthDate(LocalDate birthDate) {
--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 class WelcomeController {
 
-    private static final String MESSAGE = "> BEST DAY <";
+    private static final String MESSAGE = "> BEST DAY<";
 //    private static final String MESSAGE = "> " + Days.WEDNESDAY.name() + " <";
 
     @GetMapping("/")
@@ -16,11 +16,11 @@ class WelcomeController {
         model.addObject("day", MESSAGE);
         model.setViewName("welcome");
 //        adding a thread sleep
-        try {
-            Thread.sleep(2000);
-        }
-        catch(Exception e) {
-        }
+//        try {
+//            Thread.sleep(2000);
+//        }
+//        catch(Exception e) {
+//        }
 
         return model;
     }
